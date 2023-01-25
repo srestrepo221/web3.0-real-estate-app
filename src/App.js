92860ee454929ad389b7d5a48ae0013258b2513f
santiago restrepo <srest031@fiu.edu>
2023-01-24 20:07:42 -0500

    Updated frontend

--- src/App.js
+++ src/App.js
@@ -14,13 +14,79 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [homes, setHomes] = useState([])
 
+
+const loadBlockchainData = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  setProvider(provider)
+
+  const network = await provider.getNetwork()
+
+  const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+  const totalSupply = await realEstate.totalSupply()
+  const homes = []
+
+  for(var i = 1; i <= totalSupply; i++){
+    const uri = await realEstate.tokenURI(i)
+    const response = await fetch(uri)
+    const metadata = await response.json()
+    homes.push(metadata)
+  }
+
+  setHomes(homes)
+
+  const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
+  setEscrow(escrow)
+  //console.log(totalSupply.toString())
+  //console.log(config[network.chainId].escrow.address, config[network.chainId].escrow.address)
+
+  window.ethereum.on('accountsChanged', async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account);
+  })
+}
+
+useEffect(() => {
+  loadBlockchainData()
+}, [])
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
+
       <div className='cards__section'>
 
-        <h3>Welcome to Millow</h3>
+        <h3>Homes for you</h3>
+        
+        <hr />
+
+        <div className='cards'>
+          {homes.map((home, index) => (
+
+            <div className='card' key={index}>
+            <div className='card__image'>
+              <img src={home.image} alt="Home"/>
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value}</h4>
+                <p>
+                  <strong>1</strong> bds |
+                  <strong>2</strong> bath |
+                  <strong>3</strong> sqft
+                </p>
+              <p>1234 Doral St</p>
+            </div>
+          </div>
+
+          ))}
+          
+        </div>
 
       </div>
 
